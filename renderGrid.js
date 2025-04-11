41ed41354093e526de63275f6ed808438f0945e8
junothecat <gio.shin11@gmail.com>
2025-04-10 22:06:14 -0700

    Update tile spacing

--- renderGrid.js
+++ renderGrid.js
@@ -6,7 +6,7 @@ function toggleMenu() {
   
   const layout = [
     "blank", "data", "data", "hatch", "blank",
-    "data", "data", "hatch", "blank", "data",
+    "data", "data", "hatch", "data", "data",
     "blank", "data", "data", "data", "hatch",
     "data", "hatch", "data", "data", "blank"
   ];
