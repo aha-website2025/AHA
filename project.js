ea74c609b9d106dbb1ca28dcde06c381b65f3b29
junothecat <gio.shin11@gmail.com>
2025-04-13 17:34:18 -0700

    Add project page and scripts

--- project.js
+++ project.js
@@ -24,27 +24,27 @@ async function loadProject() {
         `
       },
       {
-        class: "tile",
+        class: "tile blank", // ← this creates an empty tile!
         style: "grid-column: 2 / 3;",
-        html: `<img src="projects/${slug}/1.jpg" alt="Photo 1" />`
+        html: ""
       },
       {
         class: "tile",
         style: "grid-column: 3 / 4;",
-        html: `<img src="projects/${slug}/2.jpg" alt="Photo 2" />`
+        html: `<img src="projects/${slug}/1.jpg" alt="Photo 1" />`
       },
       {
-        class: "description",
-        style: "", // handled in CSS: spans col 4 & rows
-        html: `<p>${project.description}</p>`
+        class: "tile",
+        style: "grid-column: 4 / 5; grid-row: 1 / span 2;",
+        html: `<img src="projects/${slug}/big1.jpg" alt="Photo 2" />`
       },
       {
         class: "tile",
         style: "grid-column: 1 / 2;",
-        html: `<img src="projects/${slug}/3.jpg" alt="Photo 3" />`
+        html: `<img src="projects/${slug}/2.jpg" alt="Photo 3" />`
       },
       {
-        class: "white-tile",
+        class: "tile",
         style: "grid-column: 2 / 3;",
         html: `
           <strong>Year completed:</strong> ${project.year_completed}<br>
@@ -54,7 +54,7 @@ async function loadProject() {
         `
       },
       {
-        class: "white-tile",
+        class: "tile",
         style: "grid-column: 3 / 4;",
         html: `
           <strong>Total floor space:</strong> ${project.floor_space_sqft} sqft<br>
@@ -62,6 +62,26 @@ async function loadProject() {
           <strong>Unit distribution:</strong> ${project.unit_distribution}<br>
           <strong># of inhabitants:</strong> ${project.inhabitants_per_unit} people/unit
         `
+      },
+      {
+        class: "tile hatch",
+        style: "grid-column: 1 / 2;", // change position as needed
+        html: ""
+      },
+      {
+        class: "tile",
+        style: "grid-column: 1 / 2;",
+        html: `<img src="projects/${slug}/3.jpg" alt="Photo 3" />`
+      },
+      {
+        class: "tile blank", // ← this creates an empty tile!
+        style: "grid-column: 2 / 3;",
+        html: ""
+      },
+      {
+        class: "description",
+        style: "grid-column: 4 / 5; grid-row: 3 / span 2;", 
+        html: `<p>${project.description}</p>`
       }
     ];
   
